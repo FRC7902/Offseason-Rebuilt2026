e08773ce5f66fd6958b4aa13d10e9e0315a74937
ACsNotThere <aaethancheung@gmail.com>
2026-08-22 16:26:12 -0400

    Added R2 Bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -120,6 +120,19 @@ public class RobotContainer {
      * - When held and shooter is ready, shuffle the hopper using the intake. Stop
      * shuffling when released
      */
+    m_operatorController.rightTrigger().whileTrue(
+      Commands.parallel(
+        m_shooterSystem.aimAndShoot(),
+        m_indexerSystem.feedFuel(),
+        m_intakeSystem.shuffleHopper().unless(() -> !m_shooterSystem.isShooterReady())
+      )
+    ).onFalse(
+      Commands.sequence(
+        m_flywheelSubsystem.setDutyCycle(0),
+        m_feederSubsystem.setDutyCycle(0),
+        m_intakeSystem.retractThenStopIntake().unless(() -> !m_shooterSystem.isShooterReady())
+      )
+    );
   }
 
   public Command getAutonomousCommand() {
