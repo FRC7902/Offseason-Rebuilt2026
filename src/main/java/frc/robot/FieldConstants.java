fdbc2a2613ee1e444201b5d0465e5658f6b198b1
deathslayer5-dev <deathslayer532@gmail.com>
2026-08-27 22:09:50 +0000

    feat: implemented getParams from LauncherCalculator.java

---
+++ src/main/java/frc/robot/FieldConstants.java
@@ -0,0 +1,27 @@
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class FieldConstants {
+  public static final double FIELD_WIDTH = 8.069;
+  public static final double FIELD_LENGTH = 16.541;
+
+  public static final Translation2d AUTO_AIM_OFFSET = new Translation2d(0.0, 0.0);
+
+  public static final Translation2d BLUE_HUB_CENTER = new Translation2d(
+    4.0218614 + Units.inchesToMeters(47.0) / 2.0,
+    FIELD_WIDTH / 2);
+
+  public static final Translation2d RED_HUB_CENTER = new Translation2d(11.9087646, 4.0345);
+
+  public static final double HUB_HEIGHT = Units.inchesToMeters(72.0);
+  public static final double HUB_WIDTH = Units.inchesToMeters(47.0);
+
+  public static final double BLUE_STARTING_LINE_X = 4.0218614;
+  public static final double RED_STARTING_LINE_X = 12.519177399999998;
+
+
+}
