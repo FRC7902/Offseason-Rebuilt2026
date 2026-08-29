d22ab18ed1fe41410cfea8eafe553381d00408a0
deathslayer5-dev <deathslayer532@gmail.com>
2026-08-28 20:09:32 -0400

    HTFX: passing fix and null check on lastHoodAngle

--- src/main/java/frc/robot/subsystems/shooter/launch_calculator/LaunchCalculator.java
+++ src/main/java/frc/robot/subsystems/shooter/launch_calculator/LaunchCalculator.java
@@ -32,15 +32,15 @@ public class LaunchCalculator {
       Angle hoodAngle,
       AngularVelocity flywheelSpeed,
       double timeOfFlight,
-      boolean passing
-  ) {}
+      boolean passing) {}
 
   private LaunchingParameters latestParameters = null;
 
-  public static double getMinTimeOfFlight(){
+  public static double getMinTimeOfFlight() {
     return timeOfFlightMap.get(minDist);
   }
-  public static double getMaxTimeOfFlight(){
+
+  public static double getMaxTimeOfFlight() {
     return timeOfFlightMap.get(maxDist);
   }
 
@@ -52,76 +52,71 @@ public class LaunchCalculator {
     Pose2d estimatedPose = SwerveDriveSubsystem.getInstance().getPose();
     boolean alliance = SwerveDriveSubsystem.getAlliance() == DriverStation.Alliance.Red;
     boolean passing =
-      (alliance) ?
-        (estimatedPose.getX() > FieldConstants.RED_STARTING_LINE_X):(estimatedPose.getX()> FieldConstants.BLUE_STARTING_LINE_X);
+        (alliance)
+            ? (estimatedPose.getX() < FieldConstants.RED_STARTING_LINE_X)
+            : (estimatedPose.getX() < FieldConstants.BLUE_STARTING_LINE_X);
     ChassisSpeeds robotRelativeVelocity = SwerveDriveSubsystem.getInstance().getRobotVelocity();
     estimatedPose =
-      estimatedPose.exp(
-        new Twist2d(
-          robotRelativeVelocity.vxMetersPerSecond * phaseDelay,
-          robotRelativeVelocity.vyMetersPerSecond * phaseDelay,
-          robotRelativeVelocity.omegaRadiansPerSecond * phaseDelay));
+        estimatedPose.exp(
+            new Twist2d(
+                robotRelativeVelocity.vxMetersPerSecond * phaseDelay,
+                robotRelativeVelocity.vyMetersPerSecond * phaseDelay,
+                robotRelativeVelocity.omegaRadiansPerSecond * phaseDelay));
     Translation2d target =
-      passing?
-      getPassingTarget() : alliance? FieldConstants.RED_HUB_CENTER : FieldConstants.BLUE_HUB_CENTER;
-    Pose2d launcherPosition = estimatedPose;//.transformBy(robotToLauncher.Transform2d());
+        passing
+            ? getPassingTarget()
+            : alliance ? FieldConstants.RED_HUB_CENTER : FieldConstants.BLUE_HUB_CENTER;
+    Pose2d launcherPosition = estimatedPose; // .transformBy(robotToLauncher.Transform2d());
     double launcherToTargetDistance = target.getDistance(launcherPosition.getTranslation());
 
     var robotVelocity = SwerveDriveSubsystem.getInstance().getFieldSetpointVelocity();
     var robotAngle = SwerveDriveSubsystem.getInstance().getRotation();
     ChassisSpeeds launcherVelocity =
-      DriverStation.isAutonomous()
-        ? robotVelocity
-        : transformVelocity(
-        robotVelocity, robotToLauncher.getTranslation().toTranslation2d(), robotAngle);
+        DriverStation.isAutonomous()
+            ? robotVelocity
+            : transformVelocity(
+                robotVelocity, robotToLauncher.getTranslation().toTranslation2d(), robotAngle);
     double timeOfFlight =
-      passing
-        ? passingTimeOfFlightMap.get(launcherToTargetDistance)
-        : timeOfFlightMap.get(launcherToTargetDistance);
+        passing
+            ? passingTimeOfFlightMap.get(launcherToTargetDistance)
+            : timeOfFlightMap.get(launcherToTargetDistance);
     Pose2d lookaheadPose = launcherPosition;
     double lookaheadLauncherToTargetDistance = launcherToTargetDistance;
 
     for (int i = 0; i < 20; i++) {
       timeOfFlight =
-        passing
-          ? passingTimeOfFlightMap.get(lookaheadLauncherToTargetDistance)
-          : timeOfFlightMap.get(lookaheadLauncherToTargetDistance);
+          passing
+              ? passingTimeOfFlightMap.get(lookaheadLauncherToTargetDistance)
+              : timeOfFlightMap.get(lookaheadLauncherToTargetDistance);
       double effectiveTOF = timeOfFlight;
       double offsetX = launcherVelocity.vxMetersPerSecond * effectiveTOF;
       double offsetY = launcherVelocity.vyMetersPerSecond * effectiveTOF;
       lookaheadPose =
-        new Pose2d(
-          launcherPosition.getTranslation().plus(new Translation2d(offsetX, offsetY)),
-          launcherPosition.getRotation());
+          new Pose2d(
+              launcherPosition.getTranslation().plus(new Translation2d(offsetX, offsetY)),
+              launcherPosition.getRotation());
       lookaheadLauncherToTargetDistance = target.getDistance(lookaheadPose.getTranslation());
     }
 
-    Pose2d lookaheadRobotPose =
-      lookaheadPose.transformBy(toTransform2d(robotToLauncher));
+    Pose2d lookaheadRobotPose = lookaheadPose.transformBy(toTransform2d(robotToLauncher));
     Rotation2d driveAngle = getDriveAngleWithLauncherOffset(lookaheadRobotPose, target);
 
     Angle turretAngle = getTurretAngleToHub(lookaheadRobotPose);
 
     Angle hoodAngle =
-      (passing
-              ? passingHoodAngleMap.get(lookaheadLauncherToTargetDistance)
-              : hoodAngleMap.get(lookaheadLauncherToTargetDistance));
+        (passing
+            ? passingHoodAngleMap.get(lookaheadLauncherToTargetDistance)
+            : hoodAngleMap.get(lookaheadLauncherToTargetDistance));
     if (lastDriveAngle == null) lastDriveAngle = driveAngle;
-    if (Double.isNaN(lastHoodAngle.in(Degrees))) lastHoodAngle = hoodAngle;
+    if (lastHoodAngle == null || Double.isNaN(lastHoodAngle.in(Degrees))) lastHoodAngle = hoodAngle;
 
     AngularVelocity flywheelVelocity =
-      passing
-        ? passingFlywheelSpeedMap.get(lookaheadLauncherToTargetDistance)
-        : flywheelSpeedMap.get(lookaheadLauncherToTargetDistance);
+        passing
+            ? passingFlywheelSpeedMap.get(lookaheadLauncherToTargetDistance)
+            : flywheelSpeedMap.get(lookaheadLauncherToTargetDistance);
 
     latestParameters =
-      new LaunchingParameters(
-        turretAngle,
-        hoodAngle,
-        flywheelVelocity,
-        timeOfFlight,
-        passing
-      );
+        new LaunchingParameters(turretAngle, hoodAngle, flywheelVelocity, timeOfFlight, passing);
     return latestParameters;
   }
 
