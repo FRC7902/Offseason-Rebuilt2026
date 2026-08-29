b2ef87c2a16a6a00c9b3342b32e4c33e30039055
deathslayer5-dev <deathslayer532@gmail.com>
2026-08-28 20:09:32 -0400

    HTFX: passing fix and null check on lastHoodAngle

--- src/main/java/frc/robot/subsystems/shooter/launch_calculator/LaunchUtil.java
+++ src/main/java/frc/robot/subsystems/shooter/launch_calculator/LaunchUtil.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems.shooter.launch_calculator;
 
+import static edu.wpi.first.units.Units.Degrees;
+import static frc.robot.subsystems.shooter.launch_calculator.LaunchConstants.*;
+import static frc.robot.subsystems.shooter.launch_calculator.LaunchConstants.robotToLauncher;
+
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.*;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
@@ -9,26 +13,22 @@ import frc.robot.FieldConstants;
 import frc.robot.subsystems.shooter.turret.TurretConstants;
 import frc.robot.subsystems.swervedrive.SwerveDriveSubsystem;
 
-import static edu.wpi.first.units.Units.Degrees;
-import static frc.robot.subsystems.shooter.launch_calculator.LaunchConstants.*;
-import static frc.robot.subsystems.shooter.launch_calculator.LaunchConstants.robotToLauncher;
-
 public class LaunchUtil {
   // Helpers
-  public static Translation2d getPassingTarget(){
+  public static Translation2d getPassingTarget() {
     return new Translation2d(xPassTarget, yPassTarget);
   }
 
   public static Pose2d getPose(Pose2d robotPose) {
     return robotPose.plus(
-      new Transform2d(
-        TurretConstants.ROBOT_TO_TURRET.getTranslation().toTranslation2d(),
-        TurretConstants.ROBOT_TO_TURRET.getRotation().toRotation2d()));
+        new Transform2d(
+            TurretConstants.ROBOT_TO_TURRET.getTranslation().toTranslation2d(),
+            TurretConstants.ROBOT_TO_TURRET.getRotation().toRotation2d()));
   }
 
   public static Angle getTurretAngleToHub(Pose2d robotPose) {
     Angle robotRotationCompensatedAngle =
-      getAngleToAllianceHub(robotPose).minus(robotPose.getRotation().getMeasure());
+        getAngleToAllianceHub(robotPose).minus(robotPose.getRotation().getMeasure());
     return wrapAngle(robotRotationCompensatedAngle);
   }
 
@@ -41,7 +41,7 @@ public class LaunchUtil {
 
   private static Translation2d getAllianceHubTranslation2d() {
     DriverStation.Alliance alliance = null;
-    if (DriverStation.getAlliance().isPresent()){
+    if (DriverStation.getAlliance().isPresent()) {
       alliance = DriverStation.getAlliance().get();
     }
     if (alliance == DriverStation.Alliance.Red) {
@@ -59,18 +59,19 @@ public class LaunchUtil {
   }
 
   public static ChassisSpeeds transformVelocity(
-    ChassisSpeeds velocity, Translation2d transform, Rotation2d currentRotation) {
+      ChassisSpeeds velocity, Translation2d transform, Rotation2d currentRotation) {
     return new ChassisSpeeds(
-      velocity.vxMetersPerSecond
-        - velocity.omegaRadiansPerSecond
-        * (transform.getX() * currentRotation.getSin()
-        + transform.getY() * currentRotation.getCos()),
-      velocity.vyMetersPerSecond
-        + velocity.omegaRadiansPerSecond
-        * (transform.getX() * currentRotation.getCos()
-        - transform.getY() * currentRotation.getSin()),
-      velocity.omegaRadiansPerSecond);
+        velocity.vxMetersPerSecond
+            - velocity.omegaRadiansPerSecond
+                * (transform.getX() * currentRotation.getSin()
+                    + transform.getY() * currentRotation.getCos()),
+        velocity.vyMetersPerSecond
+            + velocity.omegaRadiansPerSecond
+                * (transform.getX() * currentRotation.getCos()
+                    - transform.getY() * currentRotation.getSin()),
+        velocity.omegaRadiansPerSecond);
   }
+
   public static Transform2d toTransform2d(Transform3d transform3d) {
     Translation3d t = transform3d.getTranslation();
     Rotation3d r = transform3d.getRotation();
@@ -80,28 +81,32 @@ public class LaunchUtil {
 
     return new Transform2d(translation2d, rotation2d);
   }
-  public static Rotation2d getDriveAngleWithLauncherOffset(
-    Pose2d robotPose, Translation2d target) {
+
+  public static Rotation2d getDriveAngleWithLauncherOffset(Pose2d robotPose, Translation2d target) {
     Rotation2d fieldToHubAngle = target.minus(robotPose.getTranslation()).getAngle();
     Rotation2d hubAngle =
-      new Rotation2d(
-        Math.asin(
-          MathUtil.clamp(
-            robotToLauncher.getTranslation().getY()
-              / target.getDistance(robotPose.getTranslation()),
-            -1.0,
-            1.0)));
+        new Rotation2d(
+            Math.asin(
+                MathUtil.clamp(
+                    robotToLauncher.getTranslation().getY()
+                        / target.getDistance(robotPose.getTranslation()),
+                    -1.0,
+                    1.0)));
     return fieldToHubAngle.plus(hubAngle).plus(robotToLauncher.getRotation().toRotation2d());
   }
+
   public static Pose2d getStationaryAimedPose(Translation2d robotTranslation, boolean forceBlue) {
-    boolean passing =
-      LaunchCalculator.getInstance().getParameters().passing();
+    boolean passing = LaunchCalculator.getInstance().getParameters().passing();
 
     Translation2d target =
-      passing?
-        getPassingTarget() : SwerveDriveSubsystem.getAlliance() == DriverStation.Alliance.Red ? FieldConstants.RED_HUB_CENTER : FieldConstants.BLUE_HUB_CENTER;
+        passing
+            ? getPassingTarget()
+            : SwerveDriveSubsystem.getAlliance() == DriverStation.Alliance.Red
+                ? FieldConstants.RED_HUB_CENTER
+                : FieldConstants.BLUE_HUB_CENTER;
 
     return new Pose2d(
-      robotTranslation, getDriveAngleWithLauncherOffset(new Pose2d(robotTranslation, Rotation2d.kZero), target));
+        robotTranslation,
+        getDriveAngleWithLauncherOffset(new Pose2d(robotTranslation, Rotation2d.kZero), target));
   }
 }
