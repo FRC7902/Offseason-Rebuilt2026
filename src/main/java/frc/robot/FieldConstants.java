b07cad03dc9fbe99af67382bae45575aa6f02121
deathslayer5-dev <deathslayer532@gmail.com>
2026-08-28 20:09:32 -0400

    HTFX: passing fix and null check on lastHoodAngle

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -2,8 +2,6 @@ package frc.robot;
 
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
-import java.util.ArrayList;
-import java.util.List;
 
 public final class FieldConstants {
   public static final double FIELD_WIDTH = 8.069;
@@ -11,9 +9,8 @@ public final class FieldConstants {
 
   public static final Translation2d AUTO_AIM_OFFSET = new Translation2d(0.0, 0.0);
 
-  public static final Translation2d BLUE_HUB_CENTER = new Translation2d(
-    4.0218614 + Units.inchesToMeters(47.0) / 2.0,
-    FIELD_WIDTH / 2);
+  public static final Translation2d BLUE_HUB_CENTER =
+      new Translation2d(4.0218614 + Units.inchesToMeters(47.0) / 2.0, FIELD_WIDTH / 2);
 
   public static final Translation2d RED_HUB_CENTER = new Translation2d(11.9087646, 4.0345);
 
@@ -22,6 +19,4 @@ public final class FieldConstants {
 
   public static final double BLUE_STARTING_LINE_X = 4.0218614;
   public static final double RED_STARTING_LINE_X = 12.519177399999998;
-
-
 }
