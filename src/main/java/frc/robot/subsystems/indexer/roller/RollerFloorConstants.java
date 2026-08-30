1cd96699c569bd618b3b6c905d696822a2823e4f
Willie Pai <PaisWillie@gmail.com>
2026-08-30 17:59:32 -0400

    fix: tune roller floor mech

--- src/main/java/frc/robot/subsystems/indexer/roller/RollerFloorConstants.java
+++ src/main/java/frc/robot/subsystems/indexer/roller/RollerFloorConstants.java
@@ -30,7 +30,7 @@ public class RollerFloorConstants {
   public static final SmartMotorControllerConfig SMC_CONFIG =
       new SmartMotorControllerConfig()
           .withClosedLoopController(0.00016541, 0, 0) // TODO
-          .withSimClosedLoopController(0.00016541, 0, 0) // TODO
+          .withSimClosedLoopController(0, 0, 0)
           .withGearing(new MechanismGearing(GearBox.fromReductionStages(3.57142857143)))
           .withIdleMode(MotorMode.COAST) // TODO
           .withTelemetry(
@@ -44,7 +44,7 @@ public class RollerFloorConstants {
           .withClosedLoopRampRate(Seconds.of(0.25))
           .withOpenLoopRampRate(Seconds.of(0.25))
           .withFeedforward(new SimpleMotorFeedforward(0.27937, 0.089836, 0.014557)) // TODO
-          .withSimFeedforward(new SimpleMotorFeedforward(0.27937, 0.089836, 0.014557)); // TODO
+          .withSimFeedforward(new SimpleMotorFeedforward(0.0102, 0.4382, 0));
 
   public static final FlyWheelConfig FLY_WHEEL_CONFIG =
       new FlyWheelConfig()
