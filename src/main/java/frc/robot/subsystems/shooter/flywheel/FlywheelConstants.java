db0085b41ef4632f361fc269d3733c817d68ed22
FRC7902-DS <driverstation@markhamfirebirds.ca>
2026-09-07 17:01:48 -0400

    feat: tune flywheel feedforward

--- src/main/java/frc/robot/subsystems/shooter/flywheel/FlywheelConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/flywheel/FlywheelConstants.java
@@ -20,30 +20,32 @@ import yams.telemetry.SmartMotorControllerTelemetryConfig;
 
 public class FlywheelConstants {
 
-  public static final DCMotor LEADER_MOTOR = DCMotor.getKrakenX60Foc(1); // TODO
-  public static final DCMotor FOLLOWER_MOTOR = DCMotor.getKrakenX60Foc(1); // TODO
-  public static final int LEADER_CAN_ID = 6; // TODO
-  public static final int FOLLOWER_CAN_ID = 10; // TODO
+  public static final DCMotor LEADER_MOTOR = DCMotor.getKrakenX60Foc(1);
+  public static final DCMotor FOLLOWER_MOTOR = DCMotor.getKrakenX60Foc(1);
+  public static final int LEADER_CAN_ID = 18;
+  public static final int FOLLOWER_CAN_ID = 25;
+
+  public static final AngularVelocity MAX_RPM = RPM.of(5785); // TODO
 
   public static final AngularVelocity TOLERANCE = RPM.of(10); // TODO
 
   public static final SmartMotorControllerConfig LEADER_SMC_CONFIG =
       new SmartMotorControllerConfig()
-          .withClosedLoopController(0.00016541, 0, 0) // TODO
+          .withClosedLoopController(0, 0, 0) // TODO
           .withSimClosedLoopController(0, 0, 0)
           .withGearing(new MechanismGearing(GearBox.fromReductionStages(1)))
-          .withIdleMode(MotorMode.COAST) // TODO
+          .withIdleMode(MotorMode.COAST)
           .withTelemetry(
               "ShooterFlywheelMotor",
               new SmartMotorControllerTelemetryConfig()
                   .withTelemetryVerbosity(SmartMotorControllerConfig.TelemetryVerbosity.HIGH)
                   .withDataLogName("ShooterFlywheelMotor")
                   .withNetworkTables(!DriverStation.isFMSAttached()))
-          .withStatorCurrentLimit(Amps.of(40)) // TODO
+          .withStatorCurrentLimit(Amps.of(80)) // TODO
           .withMotorInverted(false) // TODO
           .withClosedLoopRampRate(Seconds.of(0.25))
           .withOpenLoopRampRate(Seconds.of(0.25))
-          .withFeedforward(new SimpleMotorFeedforward(0.27937, 0.089836, 0.014557)) // TODO
+          .withFeedforward(new SimpleMotorFeedforward(0.38435, 0.12, 0.010946))
           .withSimFeedforward(new SimpleMotorFeedforward(0.0102, 0.12269, 0))
           .withMomentOfInertia(Inches.of(4), Pounds.of(1)); // TODO
 
@@ -54,5 +56,5 @@ public class FlywheelConstants {
       new FlyWheelConfig()
           .withDiameter(Inches.of(4))
           .withTelemetry("ShooterFlywheelMech", TelemetryVerbosity.HIGH)
-          .withSpeedometerSimulation(RPM.of(5785));
+          .withSpeedometerSimulation(MAX_RPM);
 }
