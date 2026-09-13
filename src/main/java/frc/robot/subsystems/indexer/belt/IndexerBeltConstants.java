d0114251b1e63e771467a965a865504736ff5800
FRC7902-DS <driverstation@markhamfirebirds.ca>
2026-09-12 22:16:58 -0400

    fix: lower indexer belt speed

--- src/main/java/frc/robot/subsystems/indexer/belt/IndexerBeltConstants.java
+++ src/main/java/frc/robot/subsystems/indexer/belt/IndexerBeltConstants.java
@@ -29,7 +29,7 @@ public class IndexerBeltConstants {
   public static final AngularVelocity STORING_SPEED = RPM.of(100); // TODO
 
   public static final double FEEDING_DUTY_CYCLE = 1.0;
-  public static final double STORING_DUTY_CYCLE = 0.2;
+  public static final double STORING_DUTY_CYCLE = 0.05;
 
   public static final SmartMotorControllerConfig LEADER_SMC_CONFIG =
       new SmartMotorControllerConfig()
